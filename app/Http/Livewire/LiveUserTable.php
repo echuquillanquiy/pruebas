45bd006638a1284686b76d663c49f039f1e6d217
eduardo <echuquillanquiy@gmail.com>
2021-03-05 17:19:39 -0500

    Modulo Usuarios Listado y paginacion

---
+++ app/Http/Livewire/LiveUserTable.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\User;
+use Livewire\{Component, WithPagination};
+
+
+class LiveUserTable extends Component
+{
+    use WithPagination;
+
+    public $search = '';
+    public $perPage = 5;
+    public $camp = null;
+    public $order = null;
+    public $icon = '-circle';
+    protected $queryString = [
+        'search' => ['except' => ''],
+        'camp' => ['except' => null],
+        'order' => ['except' => null],
+    ];
+
+    public function render()
+    {
+        $users = User::where('name', 'like', "%{$this->search}%")
+            ->orWhere('email', 'like', "%{$this->search}%")
+            ->orWhere('username', 'like', "%{$this->search}%");
+
+        if ($this->camp && $this->order) {
+            $users = $users->orderBy($this->camp, $this->order);
+        } else {
+            $this->camp = null;
+            $this->order = null;
+        }
+
+        $users = $users->paginate($this->perPage);
+
+        return view('livewire.live-user-table', [
+            'users' => $users
+        ]);
+    }
+
+    public function mount()
+    {
+        $this->icon = $this->iconDirection($this->order);
+    }
+
+    public function clear()
+    {
+        $this->page = 1;
+        $this->order = null;
+        $this->camp = null;
+        $this->icon = '-circle';
+        $this->search = '';
+        $this->perPage = 5;
+
+    }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
+    public function sortable($camp)
+    {
+        if ($camp !== $this->camp) {
+            $this->order = null;
+        }
+        switch ($this->order) {
+            case null:
+                $this->order = 'asc';
+                break;
+            case 'asc':
+                $this->order = 'desc';
+                break;
+            case 'desc':
+                $this->order = null;
+                break;
+        }
+        $this->icon = $this->iconDirection($this->order);
+        $this->camp = $camp;
+    }
+
+    public function iconDirection($sort): string
+    {
+        if (!$sort) {
+            return '-circle';
+        }
+        return $sort === 'asc' ? '-arrow-circle-up' : '-arrow-circle-down';
+    }
+}
---
+++ resources/views/livewire/live-user-table.blade.php
@@ -0,0 +1,98 @@
+<div class="flex flex-col">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <div class="bg-white px-4 py-3 items-center justify-between border-t border-gray-200 sm:px-6">
+                    <div class="flex text-gray-500">
+                        <select class="block w-600 px-6 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" wire:model="perPage" >
+                            <option value="5">5</option>
+                            <option value="10">10</option>
+                            <option value="15">15</option>
+                            <option value="20">20</option>
+                        </select>
+                        <input type="text"
+                               class="shadow-sm sm:text-sm border-gray-300 rounded-md w-full text-gray-500 ml-6"
+                               wire:model="search"
+                               placeholder="Ingrese el termino de busqueda">
+                        <button wire:click="clear" class="ml-6">
+                            <span class="fa fa-eraser"></span>
+                        </button>
+                    </div>
+                </div>
+
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                        <tr>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                id
+                                <button wire:click="sortable('id')">
+                                    <span class="fa fa{{ $camp === 'id' ? $icon: '-circle' }}"></span>
+                                </button>
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                name
+                                <button wire:click="sortable('name')">
+                                    <span class="fa fa{{ $camp === 'name' ? $icon: '-circle' }}"></span>
+                                </button>
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Email
+                                <button wire:click="sortable('email')">
+                                    <span class="fa fa{{ $camp === 'email' ? $icon: '-circle' }}"></span>
+                                </button>
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                username
+                                <button wire:click="sortable('username')">
+                                    <span class="fa fa{{ $camp === 'username' ? $icon: '-circle' }}"></span>
+                                </button>
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Role
+                            </th>
+                            <th scope="col" class="relative px-6 py-3">
+                                <span class="sr-only">Edit</span>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                    @foreach($users as $user)
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="flex items-center">
+                                    <div class="ml-4">
+                                        <div class="text-sm font-medium text-gray-900">
+                                            {{ $user->id }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">{{ $user->name }}</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                  {{ $user->email }}
+                                </span>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">{{ $user->username }}</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                Admin
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                    <!-- More items... -->
+                    </tbody>
+                </table>
+                <div class="bg-white px-4 py-3 items-center justify-between border-t border-gray-200 sm:px-6">
+                    {{ $users->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
